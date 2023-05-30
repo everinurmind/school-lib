f5c856957ae58cf1844b276b134b6f7d651a574a
everinurmind <nurbol.sultanov@gmail.com>
2023-05-30 16:12:00 -0600

    Add test cases

--- main.rb
+++ main.rb
@@ -1,13 +1,37 @@
 require_relative 'person'
-require_relative 'decorator'
-require_relative 'trimmer_decorator'
-require_relative 'capitalize_decorator'
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'classroom'
+require_relative 'book'
+require_relative 'rental'
 
-person = Person.new(22, name: 'maximilianus')
-puts "Original name: #{person.correct_name}"
+# Creating a Classroom
+classroom = Classroom.new('Class A')
 
-capitalized_person = CapitalizeDecorator.new(person)
-puts "Capitalized name: #{capitalized_person.correct_name}"
+# Creating students and assigning them to the classroom
+Student.new(1, classroom, name: 'John', age: 16)
+Student.new(2, classroom, name: 'Jane', age: 17)
 
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts "Capitalized and trimmed name: #{capitalized_trimmed_person.correct_name}"
+puts "Classroom: #{classroom.label}"
+puts "Students in Classroom: #{classroom.students.map(&:name).join(', ')}"
+puts '---'
+
+# Creating a Book
+book = Book.new('The Great Gatsby', 'F. Scott Fitzgerald')
+
+# Creating a Person (Teacher)
+teacher = Teacher.new(100, 'English', name: 'Mr. Smith', age: 35)
+
+# Creating a Rental
+rental = Rental.new(Time.now, book, teacher)
+
+puts "Book Title: #{book.title}"
+puts "Book Author: #{book.author}"
+puts "Rented by: #{rental.person.name}"
+puts '---'
+
+# Checking rentals for a person
+puts "Rentals for #{teacher.name}:"
+teacher.rentals.each do |rental_item|
+  puts "Book: #{rental_item.book.title}, Date: #{rental_item.date}"
+end
