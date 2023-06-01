0e550e584e46d82dc30e0da14197172ebb85da71
everinurmind <nurbol.sultanov@gmail.com>
2023-06-01 00:33:26 -0600

    Implement requested changes

--- app.rb
+++ app.rb
@@ -23,11 +23,11 @@ class App
   end
 
   def create_student(age, name, parent_permission)
-    @people << Student.new(age, name, parent_permission: parent_permission)
+    @people << Student.new(age, name, parent_permission:)
   end
 
   def create_teacher(age, name, specialization)
-    @people << Teacher.new(specialization, name: name, age: age)
+    @people << Teacher.new(specialization, name:, age:)
   end
 
   def create_book(title, author)
@@ -41,14 +41,68 @@ class App
   end
 
   def list_rentals_for_person(person_id)
-    person = @people.find do |p|
-      p.id == person_id
-    end
+    person = @people.find { |p| p.id == person_id }
     return unless person
 
     puts 'Rentals'
     person.rentals.each do |rental|
       puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
     end
+  end
+
+  def create_person
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    choice = gets.chomp
+    if choice == '1'
+      create_student_from_input
+    elsif choice == '2'
+      create_teacher_from_input
+    else
+      puts 'Invalid choice'
+    end
+  end
+
+  def create_student_from_input
+    print 'Age: '
+    age = gets.chomp
+    print 'Name: '
+    name = gets.chomp
+    print 'Has parent permission? [Y/N]: '
+    permission = gets.chomp.downcase == 'y'
+    create_student(age, name, permission)
+    puts 'Person created successfully'
+  end
+
+  def create_teacher_from_input
+    print 'Age: '
+    age = gets.chomp
+    print 'Name: '
+    name = gets.chomp
+    print 'Specialization: '
+    specialization = gets.chomp
+    create_teacher(age, name, specialization)
+    puts 'Person created successfully'
+  end
+
+  def create_book_from_input
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    create_book(title, author)
+    puts 'Book created successfully'
+  end
+
+  def create_rentals_from_input
+    puts 'Select a book from the following list by number'
+    list_all_books(select: true)
+    book_idx = gets.chomp.to_i
+    puts "\nSelect a person from the following list by number (not id)"
+    list_all_people(select: true)
+    person_idx = gets.chomp.to_i
+    print "\nDate: "
+    date = gets.chomp
+    create_rental(book_idx, person_idx, date)
+    puts 'Rental created successfully'
   end
 end
--- main.rb
+++ main.rb
@@ -1,39 +1,5 @@
 require_relative './app'
 
-def create_student(app)
-  print 'Age: '
-  age = gets.chomp
-  print 'Name: '
-  name = gets.chomp
-  print 'Has parent permission? [Y/N]: '
-  permission = gets.chomp.downcase == 'y'
-  app.create_student(age, name, permission)
-  puts 'Person created successfully'
-end
-
-def create_teacher(app)
-  print 'Age: '
-  age = gets.chomp
-  print 'Name: '
-  name = gets.chomp
-  print 'Specialization: '
-  specialization = gets.chomp
-  app.create_teacher(age, name, specialization)
-  puts 'Person created successfully'
-end
-
-def create_person(app)
-  print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
-  choice = gets.chomp
-  if choice == '1'
-    create_student(app)
-  elsif choice == '2'
-    create_teacher(app)
-  else
-    puts 'Invalid choice'
-  end
-end
-
 def show_options
   puts "\nPlease choose an option by entering a number:"
   puts '1 - List all books'
@@ -43,28 +9,6 @@ def show_options
   puts '5 - Create a rental'
   puts '6 - List all rentals for a given person id'
   puts '7 - Exit'
-end
-
-def create_rentals(app)
-  puts 'Select a book from the following list by number'
-  app.list_all_books(select: true)
-  book_idx = gets.chomp.to_i
-  puts "\nSelect a person from the following list by number (not id)"
-  app.list_all_people(select: true)
-  person_idx = gets.chomp.to_i
-  print "\nDate: "
-  date = gets.chomp
-  app.create_rental(book_idx, person_idx, date)
-  puts 'Rental created successfully'
-end
-
-def create_book(app)
-  print 'Title: '
-  title = gets.chomp
-  print 'Author: '
-  author = gets.chomp
-  app.create_book(title, author)
-  puts 'Book created successfully'
 end
 
 def list_rentals(app)
@@ -84,9 +28,9 @@ def main
     options = {
       '1' => app.method(:list_all_books),
       '2' => app.method(:list_all_people),
-      '3' => -> { create_person(app) },
-      '4' => -> { create_book(app) },
-      '5' => -> { create_rentals(app) },
+      '3' => app.method(:create_person),
+      '4' => app.method(:create_book_from_input),
+      '5' => app.method(:create_rentals_from_input),
       '6' => -> { list_rentals(app) },
       '7' => lambda {
                puts 'Thank you for using this app!'
