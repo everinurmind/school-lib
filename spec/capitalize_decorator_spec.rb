77fccd420740f3f160f99ac1115748f26e487579
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-06-09 12:12:18 +0430

    Add capitalize decorator spec

---
+++ spec/capitalize_decorator_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../decorators/capitalize_decorator'
+describe CapitalizeDecorator do
+  let(:nameable) { double('Nameable') }
+  let(:capitalize) { CapitalizeDecorator.new(nameable) }
+  describe '#correct_name' do
+    it 'capitalizes the name returned by the nameable object' do
+      allow(nameable).to receive(:correct_name).and_return('john doe')
+      expect(capitalize.correct_name).to eq('John doe')
+    end
+  end
+end
